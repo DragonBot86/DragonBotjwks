5672ea1b69422108fbc8d037f2446730ee281600
OchoaBot <91572024+OchoaBot@users.noreply.github.com>
2021-11-03 17:57:34 -0500

    Add files via upload

---
+++ src/version.js
@@ -0,0 +1,22 @@
+const version = (prefix, pushname) => {
+    return `
+*XavyBot By Ochoa*
+
+*Actualizado:* 1 DE NOVIEMBRE DEL 2021
+*Versión actual:* 1.0
+*Ofrecida por:* Ochoa™
+
+
+*INFORME*
+
+Si no sirve el !kick o !pafuera revisa que version del bot tienes y revisa el blog del bot
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+
+
+_*by Ochoa*_
+`
+
+}
+
+exports.version = version
