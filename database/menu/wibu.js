2a62ee9ed4cc55c73d520f64783e9cf7d7b42974
OchoaBot <91572024+OchoaBot@users.noreply.github.com>
2021-11-03 19:03:16 -0500

    Update wibu.js

--- database/menu/wibu.js
+++ database/menu/wibu.js
@@ -1,10 +1,10 @@
 const wibu = (prefix, ownerBot, botName) => {
         return `
-「 *${XavyBot}* 」
+「 *${DragonBot}* 」
 
 ◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${Ochoa}
+  ❏ Creator: ${Dragon}
 ◪ *ABOUT*
   │
   ├─ ❏ ${prefix}info
