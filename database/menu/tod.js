1cff12cffa0da6ab31abea0d4c0076eccff5a058
OchoaBot <91572024+OchoaBot@users.noreply.github.com>
2021-11-03 19:04:17 -0500

    Update tod.js

--- database/menu/tod.js
+++ database/menu/tod.js
@@ -1,10 +1,10 @@
 const tod = (prefix, ownerBot, botName) => {
         return `
-「 *${XavyBot}* 」
+「 *${DragonBot}* 」
 
 ◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${Ochoa}
+  ❏ Creator: ${Dragon}
 ◪ *ABOUT*
   │
   ├─ ❏ ${prefix}info
